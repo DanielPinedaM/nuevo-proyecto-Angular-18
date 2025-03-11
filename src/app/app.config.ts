0aa3330b12dcf615845f7842a2ec7e10e38e6360
daniel <daniel.pinedamo@amigo.edu.co>
2025-03-10 21:52:45 -0500

    importar @ngxpert/hot-toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app-routing.module';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    provideAnimations()
+    provideAnimations(),
+    provideHotToastConfig(),
   ]
 };
